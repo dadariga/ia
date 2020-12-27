0d56dc73c7117d8e1f6500709eb80c6a20dc0440
Lukas Rieger <lukas.rieger@stud.th-deg.de>
2020-12-27 19:00:21 +0100

    19.00

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -28,5 +28,10 @@
         
     })
   
-  })
-        
+  
+  function load(){
+    document.getElementById("splashscreen").style.display='none';
+  }
+  
+  window.onload = window.setTimeout(load, 3000);
+})
