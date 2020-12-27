c591f1df2be44557eb2a66f612373d79b1e1c512
1Legma <63910604+1Legma@users.noreply.github.com>
2020-12-27 18:44:39 +0100

    JS for hiding Splashscreen after loading

--- src/js/load.js
+++ src/js/load.js
@@ -1,17 +1,5 @@
-function onReady(callback) {
-    var intervalId = window.setInterval(function() {
-      if (document.getElementsByTagName('body')[0] !== undefined) {
-        window.clearInterval(intervalId);
-        callback.call(this);
-      }
-    }, 1000);
-  }
-  
-  function setVisible(selector, visible) {
-    document.querySelector(selector).style.display = visible ? 'block' : 'none';
-  }
-  
-  onReady(function() {
-    setVisible('.page', true);
-    setVisible('#loading', false);
-  });
+/*function load(){
+  document.getElementById("splashscreen").style.display='none';
+}
+
+window.onload = window.setTimeout(load, 3000);
