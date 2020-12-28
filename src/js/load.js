2228eef4a63e255eee665f24fc8883e8a804c009
Lukas Rieger <lukas.rieger@stud.th-deg.de>
2020-12-28 16:02:17 +0100

    16.02

--- src/js/load.js
+++ src/js/load.js
@@ -2,4 +2,13 @@
   document.getElementById("splashscreen").style.display='none';
 }
 
-window.onload = window.setTimeout(load, 3000);
+window.onload = window.setTimeout(load, 3000);*/
+function load(){
+  document.getElementById("splashscreen").style.display='none';
+}
+
+window.onload = window.setTimeout(load, 4000);
+
+if(screen.availHeight > screen.availWidth){
+  alert("Please use Landscape-Mode!");
+}
