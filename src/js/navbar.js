eca4cdb52e88f9fad48cb6d0b401b9b1b1865a35
Lukas Rieger <lukas.rieger@stud.th-deg.de>
2021-01-15 13:47:34 +0100

    13.47

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -99,13 +99,17 @@
     init: function () {
       this.el.sceneEl.addEventListener('markerFound', () => {
         $("#infobutton").css("display", "block");
+        $(".arrowtoleft").css("display","block");
+        $(".arrowtoright").css("display","block");
 		if (textshown){
 			$(".projektfeld").css("display", "block");
 		}
       })
 	  this.el.sceneEl.addEventListener('markerLost', () => {
 		$("#infobutton").css("display", "none");
-		$(".projektfeld").css("display", "none");
+    $(".projektfeld").css("display", "none");
+    $(".arrowtoleft").css("display","none");
+        $(".arrowtoright").css("display","none");
       })
     }
   });
@@ -186,5 +190,5 @@ window.open("navbar.html", "_self");
 })
 
 $(".arrowtoleft").click(function(){
-  window.open("markerwithvideo.html")
+  window.open("markerwithvideo.html","_self");
 })
