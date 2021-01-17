d98a0955e9854c4b5f83232f3cdbef41d9d07be9
1Legma <63910604+1Legma@users.noreply.github.com>
2021-01-17 19:45:44 +0100

    Update load.js

--- srcVersion21-01-17/js/load.js
+++ srcVersion21-01-17/js/load.js
@@ -2,7 +2,7 @@ function load(){
   document.getElementById("splashscreen").style.display='none';
 }
 
-window.onload = window.setTimeout(load, 1500);
+window.onload = window.setTimeout(load, 2000);
 
 if(screen.availHeight > screen.availWidth){
   alert("Bitte Querformat verwenden!");
